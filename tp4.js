fa9df75ff75977081b077c02a24314feb5592964
Lucas1234567890123467890 <lucaslongo99@gmail.com>
2025-04-23 15:02:09 -0300

    Update tp4.js

--- tp4.js
+++ tp4.js
@@ -1,2 +1,11 @@
-var letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K',
-    'E', 'T'];
+var letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'];
+let dni = parseInt(prompt("Numero DNI"));
+
+if(dni <=0 || dni > 99999999){
+   document.write("Este numero no es valido")
+}
+else{
+   let resto = dni % 23
+   let letra = letras[resto]
+    document.write(letra)
+}
