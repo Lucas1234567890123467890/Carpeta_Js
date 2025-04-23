3c383c623a83c068a3e54d6a40e3da737e5006f8
Lucas1234567890123467890 <lucaslongo99@gmail.com>
2025-04-23 15:19:20 -0300

    Update tp10.js

--- tp10.js
+++ tp10.js
@@ -1,10 +1,16 @@
-const resultados = Array(13).fill(0);
-
-for (let i = 0; i < 36000; i++) {
-    const dado1 = Math.floor(Math.random() * 6) + 1;
-    const dado2 = Math.floor(Math.random() * 6) + 1;
-    const suma = dado1 + dado2;
-    resultados[suma]++;
+var resultados = [];
+for (let i = 0; i < 13; i++) {
+    resultados[i] = 0;
 }
 
-console.log(resultados.slice(2));
+for (let i = 0; i < 36000; i++){
+     var dado1 = math.floor(math.random() * 6) +1;
+     var dado2 = math.floor(math.random() * 6) +1;
+     var suma = dado1 + dado2;
+
+     resultados[suma]++;
+}
+
+for (let i = 2; i <=12; i++) {
+     document.write(`Suma ${i}: ${resultados[i]} veces`);
+}
